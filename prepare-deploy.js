81ff437521258c09fc89c7849dacb05bf4fc712e
Jani Eväkallio <jani.evakallio@gmail.com>
2017-03-17 11:30:24 +0000

    Initial iteration on publish scripts

---
+++ prepare-deploy.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var pkg = require('./package.json');
+
+var branchName = process.argv[2];
+if (!branchName) {
+  throw new Error('Branch name not provided');
+}
+var sanitized = branchName.replace('/', '--');
+if (pkg.name.endsWith(sanitized)) {
+  console.log('Branch name already appended');
+} else {
+  fs.writeFileSync(
+    './package.json',
+    JSON.stringify(Object.assign(pkg, { name: pkg.name + '_' + sanitized }), null, 2)
+  );
+}
