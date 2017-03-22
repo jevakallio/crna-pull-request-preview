025cd7349ebb984724a5d26eeb4cfb53b51ff93b
Jani Eväkallio <jani.evakallio@gmail.com>
2017-03-22 11:57:46 +0000

    Change header text

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>This app has been updated via Circle CI!</Text>
+        <Text>Again!</Text>
       </View>
     );
   }
